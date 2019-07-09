b5c0cca397e673df8eda3e3265ac0040ab9228fc
s.v.taranenko <sergey.taranenko@gmail.com>
2019-07-09 17:42:25 +0700

    First skeleton

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # scrap_buildings_type_and_address
 
-What this
+<h1>What this</h1>
 Scrapping type buildings and address from site https://prawdom.ru/k_seria.php?d=progjekt_docs/s1-447.php&s=3&r=99050
+
+![specification](specification.png)
--- get_type_and_address.py
+++ get_type_and_address.py
@@ -1,6 +1,27 @@
 import pandas as pd
 import os
+from bs4 import BeautifulSoup
+import requests
+
+ROOT_URL = 'https://prawdom.ru'
+START_URL = '/k_seria.php?d=progjekt_docs/s1-447.php&s=3&r=99050'
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_date(html):
+    soup = BeautifulSoup(html, 'lxml')
+    series = soup.find('div', id='Left-Content').findAll('li')
+    return series
+
+
+def main():
+    for s in get_date(get_html(ROOT_URL + START_URL)):
+        print([s.find('a').get('href'), s.find('a').text])
 
 
 if __name__ == '__main__':
-    pass
+    main()
